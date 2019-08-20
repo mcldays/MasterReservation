875424dd81111e6330c285c9725bc00ae4239597
krendel338 <lexlex97@mail.ru>
2019-08-20 18:33:23 +0400

    Доделал шапку PersonalPage

--- MasterReservation/Scripts/PersonalDataScripts.js
+++ MasterReservation/Scripts/PersonalDataScripts.js
@@ -68,4 +68,15 @@
         e.stopPropagation();
     });
 
+    // Открытие дропдовна в шапке
+    $("#user-dropdown").slideUp(0);
+    $("#userblock").on("click", function () {
+        if ($("#user-dropdown").is(':visible')) {
+            $("#user-dropdown").slideUp(300);
+        }
+        else {
+            $("#user-dropdown").slideDown(300);
+        }
+    });
+
 });
