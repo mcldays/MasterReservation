eb409be27ecab198e1184b235db58c74a1324f18
krendel338 <lexlex97@mail.ru>
2019-08-20 20:01:08 +0400

    Доделал MainPage

--- MasterReservation/Scripts/MainPage.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function() {
-    $("input[name='City']").each(function() {
-        $(this).autocomplete({
-            source: '/Test/TestMethod'
-        });
-    });
-})
--- MasterReservation/Scripts/MainPageScripts.js
+++ MasterReservation/Scripts/MainPageScripts.js
@@ -114,4 +114,32 @@
         $("#input-offers").trigger("keyup");
     });
 
+    //автокомплит
+    $("input[name='City']").each(function () {
+        $(this).autocomplete({
+            source: '/Test/TestMethod'
+        });
+    });
+
+
+    document.getElementById('picField').onchange = function (evt) {
+        var tgt = evt.target || window.event.srcElement,
+            files = tgt.files;
+
+        // FileReader support
+        if (FileReader && files && files.length) {
+            var fr = new FileReader();
+            fr.onload = function () {
+                document.getElementById("pic").src = fr.result;
+            }
+            fr.readAsDataURL(files[0]);
+        }
+
+        // Not supported
+        else {
+            // fallback -- perhaps submit the input to an iframe and temporarily store
+            // them on the server until the user's session ends.
+        }
+    }
+
 });
