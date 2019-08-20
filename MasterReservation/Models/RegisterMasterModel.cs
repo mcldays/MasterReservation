9a17faee14d7c849ec5ed134eeb7a784f9304e58
Mcldays <mcldays@gmail.com>
2019-08-20 17:18:09 +0400

    Фиксация, удалил

--- MasterReservation/Models/RegisterMasterModel.cs
+++ MasterReservation/Models/RegisterMasterModel.cs
@@ -30,6 +30,8 @@ namespace MasterReservation.Models
 
         public string City { get; set; }
 
+
+
         [Required(ErrorMessage = "Выберите услуги")]
         public string Offers { get; set; }
 
---
+++ MasterReservation/Utilities/RequestCity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using MasterReservation.Models;
+using Newtonsoft.Json;
+
+namespace MasterReservation.Utilities
+{
+    public class RequestCity
+    {
+        private async Task<CitiesModel> Request(string term)
+        {
+            HttpClient request =
+                new HttpClient();
+            return await await request
+                .GetAsync("https://kladr-api.ru/api.php?query=" + term + "&contentType=city&withParent=0&limit=5")
+                .ContinueWith(async r =>
+                {
+                    if (r.IsFaulted)
+                        return null;
+                    CitiesModel x =
+                        JsonConvert.DeserializeObject<CitiesModel>(await r.Result.Content.ReadAsStringAsync());
+                    return x;
+                });
+
+
+
+        }
+    }
+}
